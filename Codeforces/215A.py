bbe99f7bf21e0ec56662974f2a615a51a04b069b
Victor Peng <ziliangdotme@gmail.com>
2012-08-12 21:36:04 +0930

    fixed some indention

--- Codeforces/215A.py
+++ Codeforces/215A.py
@@ -1,26 +1,25 @@
 
 def read_array(convertor=None):
-  ret = raw_input().split()
-  if convertor: ret = map(convertor, ret)
-  return ret
+    ret = raw_input().split()
+    if convertor: ret = map(convertor, ret)
+    return ret
 
 
 def main():
-	n, ns, m ,ms = input(), read_array(int), input(), read_array(int)
-	ans = 0
-	cnt = 0
-	for i in ns:
-		for j in ms:
-			if j % i == 0:
-				tmp = j / i
-				if tmp > ans:
-					ans = tmp
-					cnt = 0
-				if tmp == ans:
-					cnt += 1
+    n, ns, m ,ms = input(), read_array(int), input(), read_array(int)
+    ans = 0
+    cnt = 0
+    for i in ns:
+        for j in ms:
+            if j % i == 0:
+                tmp = j / i
+                if tmp > ans:
+                    ans = tmp
+                    cnt = 0
+                if tmp == ans:
+                    cnt += 1
 
-	print cnt
-
+    print cnt
 
 
 main()
--- Codeforces/header.py
+++ Codeforces/header.py
@@ -1,12 +1,12 @@
 
 def read_array(convertor=None):
-  ret = raw_input().split()
-  if convertor: ret = map(convertor, ret)
-  return ret
+    ret = raw_input().split()
+    if convertor: ret = map(convertor, ret)
+    return ret
 
 
 def main():
-  pass
+    pass
 
 
 
