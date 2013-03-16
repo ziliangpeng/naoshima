209915beeadbfe14433781e8d9ff34c1aefb529d
Victor Peng <ziliangdotme@gmail.com>
2013-03-16 23:49:17 +1030

    a bash script to create file for codeforces

---
+++ Codeforces/create.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+
+MSG='expected format: create -l python -f 42A'
+
+echo ''
+
+while test $# -gt 0; do
+	case "$1" in
+		-l)
+			shift
+			if test $# -gt 0; then
+				LANGUAGE=$1
+			else
+				echo $MSG
+				exit 1
+			fi
+			shift
+			;;
+
+		-f)
+			shift
+			if test $# -gt 0; then
+				FILENAME=$1
+			else
+				echo $MSG
+				exit 1
+			fi
+			shift
+			;;
+		*)
+			echo MSG
+			exit 1
+			;;
+	esac
+done
+
+if test -z "$LANGUAGE" || test -z "$FILENAME"; then
+	echo $MSG
+	exit 1
+fi
+
+FILENAME=$(echo $FILENAME | tr '[a-z]' '[A-Z]') # toupper
+echo 'FILENAME IS:' $FILENAME
+
+LANGUAGE=$(echo $LANGUAGE | tr '[A-Z]' '[a-z]') # tolower
+case "$LANGUAGE" in
+	python|py)
+		suffix='py'
+		;;
+	cpp|c++)
+		suffix='cpp'
+		;;
+esac
+if test -z "$suffix"; then
+	echo 'language '"$LANGUAGE"' not supported'
+	exit 1
+else
+	echo 'LANGUAGE IS:' $suffix
+fi
+
+template='header.'$suffix
+destination='All/'$FILENAME'.'$suffix
+echo 'copying '$template' to '$destination
+cp $template $destination
