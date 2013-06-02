0672ade0dce3d55bdf60f8f2e62e78457fd74ad0
Victor <ziliangdotme@gmail.com>
2013-06-02 16:19:08 +0930

    srm cpp template

--- SRM/cpp.ket.cc
+++ SRM/cpp.ket.cc
@@ -15,16 +15,16 @@ using namespace std;
 
 #define REP(A, B) for (int A = 0; A < B; ++A)
 #define FOR(A, B, C) for (int A = B; A < C; ++A)
-#define SZ(A) (A.size())
-#define PB push_back
-#define ALL(X) (X).begin(), (X).end()
-#define ZERO(v) memset(v, 0, sizeof(v))
+#define len(A) (A.size())
+#define pb push_back
+#define all(X) (X).begin(), (X).end()
+#define clear(v) memset(v, 0, sizeof(v))
 #define debug(X) cout<<"  ... "#X" : "<<(X)<<"\n" 
 #define uniq(A) sort(ALL(A)), (A).erase(unique((A).begin(),(A).end()),(A).end()) 
 #define conv(A,B) {stringstream _3xian;_3xian<<A;_3xian>>B;} 
-typedef long long LL;
-typedef vector<int> VI;
-typedef string STR;
+typedef long long ll;
+typedef vector<int> vi;
+typedef string str;
 
 class <%:class-name%> {
 public:
