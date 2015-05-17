c1fa990128d4a6716863e7d7e8150ec429428ec1
Ziliang Peng <ziliangdotme@gmail.com>
2015-05-17 11:55:12 -0700

    some more ruby basic play

--- begin.ruby/basic.rb
+++ begin.ruby/basic.rb
@@ -1,7 +1,11 @@
 cmd = [
     '1.methods',
+    '"Abc".methods',
     '1.even?',
-    '2.next']
+    '2.next',
+    '3.class',
+    '"xoxo".class'
+    ]
 
 cmd.each do |command|
     puts "==> Command: #{command}"
