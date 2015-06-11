51addd356bfc6058cd9e8ee5cedb8b89f8b017b9
Ziliang Peng <ziliangdotme@gmail.com>
2015-06-10 19:35:55 -0700

    Dockerized some servers/clients

--- netIO/ServerSimpleLoop/server_simple_loop.py
+++ netIO/ServerSimpleLoop/server_simple_loop.py
@@ -7,7 +7,7 @@ PORT = 5000
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 s.bind((HOST, PORT))
-s.listen(1)
+s.listen(1024)
 print 'Server is up. Listening...'
 while True:
     conn, addr = s.accept()
