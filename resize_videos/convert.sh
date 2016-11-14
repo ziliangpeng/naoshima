ec849444c1d86c80794e5b74fb62fae678760e12
Victor Peng <victor.peng@airbnb.com>
2016-11-14 01:51:35 -0800

    [script] resize videos with ffmpeg

---
+++ resize_videos/convert.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+
+OUTPUT_RESOLUTION=$1
+
+function convert_to {
+  filename=$1
+  res=$2
+  echo ">>>Starting" `date`
+  echo "converting '$filename' to $res"
+
+  tmp_file=$filename-converted-to-$res-tmp.mov
+  target_file=$filename-converted-to-$res.mov
+  stdout_file=$filename-converted-to-$res.out
+  stderr_file=$filename-converted-to-$res.err
+
+  if [ -f "$target_file" ]; then
+    echo "skipping..."
+  else
+    echo "working..."
+    echo `date` >>"$stdout_file"
+    echo `date` >>"$stderr_file"
+
+    ffmpeg -i "$filename" -vf scale=-1:$res "$tmp_file" 1>>"$stdout_file" 2>>"$stderr_file"
+    mv "$tmp_file" "$target_file"
+  fi
+  echo ">>>>>Ending" `date` "$filename"
+}
+
+log_file="./convert.log"
+echo "======= START =======" >> $log_file
+
+for file in "."/*.mov
+do
+  echo $file | grep "converted" > /dev/null
+  if [ $? -ne 0 ]; then
+    convert_to "$file" $OUTPUT_RESOLUTION >> $log_file
+  fi
+done
