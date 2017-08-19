28869a20953bd3126bebaccc16a386a355b66fbf
Victor Peng <victor.peng@airbnb.com>
2017-08-19 15:34:25 -0700

    play with java 8 lambda and stream

--- lang/java8/src/main/java/exp/naoshima/eight/Eight.java
+++ lang/java8/src/main/java/exp/naoshima/eight/Eight.java
@@ -1,7 +1,29 @@
 package exp.naoshima.eight;
 
+import java.util.function.IntUnaryOperator;
+import java.util.stream.IntStream;
+
 class Eight {
-  public static void main(String[] args) {
-    System.out.println("java 8 project compiled by bazel");
-  }
+
+    public static void main(String[] args) {
+        int value = 0;
+
+        // Functionally reduce. Apply lambda to an initial value n times
+        value = IntStream.range(0, 4).reduce(value, (a, b) -> manipulateInt(a, v -> v + 1));
+
+        value = IntStream.range(0, 4).reduce(value, (a, b) -> manipulateInt(a, v -> v * 2));
+
+        System.out.println("Final value is " + value);
+    }
+
+    /*
+     * Apply a lambda (op) to a value
+     */
+    private static int manipulateInt(int value, IntUnaryOperator op) {
+        System.out.println("Before value: " + value);
+        int ret = op.applyAsInt(value);
+        System.out.println("After value:  " + ret);
+        System.out.println("=====");
+        return ret;
+    }
 }
