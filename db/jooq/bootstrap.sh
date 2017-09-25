b2607c132bc7972027f3f17ca76fd1c00246bfb9
Victor Peng <victor.peng@airbnb.com>
2017-09-25 15:45:17 -0700

    change database from seto to banana

--- db/jooq/bootstrap.sh
+++ db/jooq/bootstrap.sh
@@ -4,18 +4,18 @@ run() {
   mysql -uroot -ppassword -e "$1" 2&>1 | grep -v "Using a password on the command line interface can be insecure"
 }
 
-run_seto() {
+run_banana() {
   mysql -uroot -ppassword seto -e "$1" 2&>1 | grep -v "Using a password on the command line interface can be insecure"
 }
 
-echo "Drop Database seto"
-run "DROP DATABASE IF EXISTS seto;"
+echo "Drop Database banana"
+run "DROP DATABASE IF EXISTS banana;"
 
-echo "Create Database seto"
-run "CREATE DATABASE seto;" # Seto inland sea
+echo "Create Database banana"
+run "CREATE DATABASE banana;"
 
 echo "Create Table Islands"
-run_seto "CREATE TABLE Islands (\
+run_banana "CREATE TABLE Islands (\
   id INT NOT NULL AUTO_INCREMENT, \
   name VARCHAR(64), \
   year INT, \
@@ -23,12 +23,12 @@ run_seto "CREATE TABLE Islands (\
   PRIMARY KEY (id));"
 
 echo "Populate Table Islands"
-run_seto "INSERT INTO Islands (id, name, year, population) VALUES (1, 'naoshima', 1987, 3000)"
-run_seto "INSERT INTO Islands (id, name, year, population) VALUES (2, 'teshima', 1988, 2000)"
-run_seto "INSERT INTO Islands (id, name, year, population) VALUES (3, 'shodoshima', 1979, 7000)"
+run_banana "INSERT INTO Islands (id, name, year, population) VALUES (1, 'naoshima', 1987, 3000)"
+run_banana "INSERT INTO Islands (id, name, year, population) VALUES (2, 'teshima', 1988, 2000)"
+run_banana "INSERT INTO Islands (id, name, year, population) VALUES (3, 'shodoshima', 1979, 7000)"
 
 echo "Create Table Arts"
-run_seto "CREATE TABLE Arts (\
+run_banana "CREATE TABLE Arts (\
   id INT NOT NULL AUTO_INCREMENT, \
   island_id INT NOT NULL, \
   name VARCHAR(64), \
@@ -38,7 +38,7 @@ run_seto "CREATE TABLE Arts (\
   );"
 
 echo "Populate Table Arts"
-run_seto "INSERT INTO Arts (id, island_id, name, price) \
+run_banana "INSERT INTO Arts (id, island_id, name, price) \
   VALUES (1, 1, 'Chi Chu Museum', 20)"
-run_seto "INSERT INTO Arts (id, island_id, name, price) \
+run_banana "INSERT INTO Arts (id, island_id, name, price) \
   VALUES (2, 2, 'Teshima Art Museum', 21)"
