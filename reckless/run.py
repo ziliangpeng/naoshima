9901e62e8fdd00a8f0fb0cc0fbbde3e0d2327fee
Victor Peng <victor.peng@airbnb.com>
2017-10-01 18:00:57 -0700

    [reckless] set up simple web.py server

---
+++ reckless/run.py
@@ -0,0 +1,17 @@
+import web
+
+
+urls = (
+    '/', 'index'
+)
+
+
+class index:
+    def GET(self):
+        raise web.seeother('/static/main.html')
+
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.internalerror = web.debugerror
+    app.run()
