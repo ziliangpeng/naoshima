10120a81b02fb2bac0578d780cf5fedcee67469e
Victor Peng <victor.peng@airbnb.com>
2017-11-05 13:46:12 -0800

    [ig] see who hasn't follow back

---
+++ instagram/who_not_follow.py
@@ -0,0 +1,11 @@
+import auth
+import utils
+
+
+bot = auth.auth()
+follows = utils.get_follows(bot)
+followers = utils.get_followers(bot)
+
+for id in follows.keys():
+    if id not in followers:
+        print id, 'https://www.instagram.com/%s/' % (follows[id])
