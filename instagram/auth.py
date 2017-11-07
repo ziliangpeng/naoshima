a2f4741bcf9666c07183c1233f44422ff5aaf27c
Victor Peng <victor.peng@airbnb.com>
2017-11-06 17:17:54 -0800

    [ig] score.py don't print bot log

--- instagram/auth.py
+++ instagram/auth.py
@@ -5,7 +5,7 @@ sys.path.append(os.path.join(sys.path[0], 'instabot_libs/src'))
 from instabot import InstaBot
 
 
-def auth():
+def auth(log_mod=0):
     login, password = secret_reader.load_secrets()
     # use instabot
-    return InstaBot(login=login, password=password, log_mod=0)
+    return InstaBot(login=login, password=password, log_mod=log_mod)
