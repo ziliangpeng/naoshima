048e5fa697048948508fc266d12663659dcfb549
Victor Peng <victor.peng@airbnb.com>
2017-11-10 23:33:00 -0800

    [ig] remove unnecessary SIGINT catcher

--- instagram/bot.py
+++ instagram/bot.py
@@ -19,13 +19,6 @@ id_name_dict = {}
 poked = set()  # ppl I've followed before
 
 
-def signal_handler(signal, frame):
-    sys.exit(0)
-
-
-signal.signal(signal.SIGINT, signal_handler)
-
-
 if __name__ == '__main__':
     # Always DoFo
     t = DoFo(bot, queue_to_fo, id_name_dict)
