abeb03e728eafeeb0833aaf941dd8ab743f41c82
Victor Peng <victor.peng@airbnb.com>
2017-11-11 19:48:11 -0800

    [twitter] catch exception

--- twitter-cli/follow_1.py
+++ twitter-cli/follow_1.py
@@ -16,6 +16,7 @@ print "found", len(following_ids), "following"
 
 name = sys.argv[1]
 print 'username is', name
+i = 0
 for l in tweepy.Cursor(api.lists_memberships, screen_name=name).items():  # lists a user is added to
     print "List:", l.name
     c = raw_input('follow?:')
@@ -23,30 +24,32 @@ for l in tweepy.Cursor(api.lists_memberships, screen_name=name).items():  # list
         continue
 
     list_id = l.id
-    i = 0
-    for user in tweepy.Cursor(api.list_members, list_id=list_id).items():
-        try:
-            if int(user.id) in following_ids:
-                print "already following", user.id, user.name
-                continue
-            if user.protected:
-                print "User", user.id, user.name, "is protected"
-                continue
-            if followed.is_followed(user.id):
-                print "already followed", user.id, user.name, "before"
-                continue
-            last_tweeted = user.status.created_at
-            if last_tweeted < datetime.now() - timedelta(days = 3):
-                print 'User %s last tweeted at %s, is too old' % (user.name, last_tweeted)
-            else:
-                print datetime.now(), "To follow user:", i, user.id, user.name
-                i += 1
+    try:
+        for user in tweepy.Cursor(api.list_members, list_id=list_id).items():
+            try:
+                if int(user.id) in following_ids:
+                    print "already following", user.id, user.name
+                    continue
+                if user.protected:
+                    print "User", user.id, user.name, "is protected"
+                    continue
+                if followed.is_followed(user.id):
+                    print "already followed", user.id, user.name, "before"
+                    continue
+                last_tweeted = user.status.created_at
+                if last_tweeted < datetime.now() - timedelta(days = 3):
+                    print 'User %s last tweeted at %s, is too old' % (user.name, last_tweeted)
+                else:
+                    print datetime.now(), "To follow user:", i, user.id, user.name
+                    i += 1
 
-                api.create_friendship(user.id)
+                    api.create_friendship(user.id)
 
-            if i >= 989:
-                print "Already used up today's quote. break."
-                break
-        except Exception as e:
-            print e
-        time.sleep(1)
+                if i >= 989:
+                    print "Already used up today's quote. break."
+                    break
+            except Exception as e:
+                print e
+            time.sleep(1)
+    except Exception as e:
+        print e
