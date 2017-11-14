836d83715b23373719675a3df4c2bb7217e668e9
Victor Peng <victor.peng@airbnb.com>
2017-11-13 21:22:31 -0800

    [ig] ec2 plot

---
+++ instagram/ec2-plot.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+python plot-likes.py \
+    ~/instagram_likes.txt \
+    ~/eva_instagram_likes.txt \
+    ~/z_instagram_likes.txt \
+    ~/newdone_instagram_likes.txt \
+    ~/nesign_instagram_likes.txt \
+    ~/hixchan_instagram_likes.txt
+
+python plot-all.py \
+    ~/instagram_followers_count.txt \
+    ~/eva_instagram_followers_count.txt \
+    ~/z_instagram_followers_count.txt \
+    ~/newdone_instagram_followers_count.txt \
+    ~/nesign_instagram_followers_count.txt \
+    ~/hixchan_instagram_followers_count.txt
