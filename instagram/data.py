d273911ef23ba993a4189fe19faf007296002a73
Victor Peng <victor.peng@airbnb.com>
2017-11-22 17:45:33 -0800

    [ig] enforce redis present

--- instagram/data.py
+++ instagram/data.py
@@ -13,29 +13,23 @@ JSON_TTL = 3600 * 24 * 7  # in seconds
 
 redis_host = secret_reader.load_redis_host()
 redis_port = secret_reader.load_redis_port()
-
 _redis = None
 if redis_host != None and redis_port != None:
     _redis = redis.Redis(redis_host, redis_port, REDIS_DB)
-
+else:
+    raise Exception("redis config must present!")
 
 
 def get_json_by_username(u):
-    if _redis == None:
-        return None
+    j = _redis.get(NAMESPACE_JSON + str(u))
+    if j != None:
+        return json.loads(j)
     else:
-        j = _redis.get(NAMESPACE_JSON + str(u))
-        if j != None:
-            return json.loads(j)
-        else:
-            return None
+        return None
 
 
 def set_json_by_username(u, j):
-    if _redis == None:
-        return None
-    else:
-        _redis.set(NAMESPACE_JSON + str(u), json.dumps(j), JSON_TTL)
+    _redis.set(NAMESPACE_JSON + str(u), json.dumps(j), JSON_TTL)
 
 
 data_filename = 'followed.txt'
