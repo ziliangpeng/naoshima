220b162807aea688ac93c272aed84e59fd0bd49c
Victor Peng <victor.peng@airbnb.com>
2017-11-30 22:38:12 -0800

    [leetcode][golang] go fmt

--- leetcode/golang/1.go
+++ leetcode/golang/1.go
@@ -3,19 +3,19 @@ package main
 import "fmt"
 
 func twoSum(nums []int, target int) []int {
-  for i := 0; i < len(nums); i++ {
-    for j := i + 1; j < len(nums); j++ {
-      if nums[i] + nums[j] == target {
-        return []int{i, j}
-      }
-    }
-  }
-  return []int{0, 0}
+	for i := 0; i < len(nums); i++ {
+		for j := i + 1; j < len(nums); j++ {
+			if nums[i]+nums[j] == target {
+				return []int{i, j}
+			}
+		}
+	}
+	return []int{0, 0}
 }
 
 func main() {
-    nums := []int {1,2,3,4,5}
-    target := 6
+	nums := []int{1, 2, 3, 4, 5}
+	target := 6
 
-    fmt.Println(twoSum(nums, target))
+	fmt.Println(twoSum(nums, target))
 }
