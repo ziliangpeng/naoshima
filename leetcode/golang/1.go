df34446bf2ae269744e5eef567743b06a3d78eed
Victor Peng <victor.peng@airbnb.com>
2017-11-30 21:21:51 -0800

    [leetcode][golang] 1

---
+++ leetcode/golang/1.go
@@ -0,0 +1,21 @@
+package main
+
+import "fmt"
+
+func twoSum(nums []int, target int) []int {
+  for i := 0; i < len(nums); i++ {
+    for j := i + 1; j < len(nums); j++ {
+      if nums[i] + nums[j] == target {
+        return []int{i, j}
+      }
+    }
+  }
+  return []int{0, 0}
+}
+
+func main() {
+    nums := []int {1,2,3,4,5}
+    target := 6
+
+    fmt.Println(twoSum(nums, target))
+}
