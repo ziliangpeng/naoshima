1d4e4183ae90aa89b09c71197c828ed1eeab429d
Victor Peng <victor.peng@airbnb.com>
2017-12-09 16:24:25 -0800

    [ig] auth will look up redis for saved sessions

--- instagram/auth.py
+++ instagram/auth.py
@@ -1,8 +1,43 @@
 import secret_reader
+import requests
+from requests.cookies import RequestsCookieJar
 from lib.instabot import InstaBot
+from http import cookiejar
+import data
 
 
-def auth(log_mod=0):
+def auth(log_mod=0, session=None):
     login, password = secret_reader.load_secrets()
-    # use instabot
-    return InstaBot(login=login, password=password, log_mod=log_mod)
+
+    if session:
+        print('auth: using session in param to create bot')
+        bot =  InstaBot(login=login, password=password, session=session, log_mod=log_mod)
+    else:
+        saved_session = data.get_session(login)
+        if saved_session:
+            print('auth: using session in redis to create bot')
+            bot =  InstaBot(login=login, password=password, session=saved_session, log_mod=log_mod)
+        else:
+            print('auth: using user/password to create bot')
+            bot =  InstaBot(login=login, password=password, log_mod=log_mod)
+
+    if bot.login_status:
+        print('auth: done. saving session into redis')
+        data.save_session(login, bot.s)
+    else:
+        print('auth: loging failed')
+
+    return bot
+
+
+def auth_from_cookies_file(filename, log_mod=0):
+    cookies = cookiejar.MozillaCookieJar(filename)
+    cookies.load()
+
+    requests_cookies = RequestsCookieJar()
+    for c in cookies:
+        requests_cookies.set_cookie(c)
+    session = requests.Session()
+    session.cookies = requests_cookies
+    return auth(log_mod=log_mod, session=session)
+
