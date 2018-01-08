c3372518558b1d64d5ac28425e660036af17fa8f
Victor Peng <victor.peng@airbnb.com>
2018-01-08 13:38:34 -0800

    [ig] adds let to plot

--- instagram/ec2-plot.sh
+++ instagram/ec2-plot.sh
@@ -4,6 +4,7 @@ python plot-likes.py \
     /data/ig/v_likes.txt \
     /data/ig/eva_likes.txt \
     /data/ig/z_likes.txt \
+    /data/ig/let_likes.txt \
     /data/ig/newdone_likes.txt \
     /data/ig/nesign_likes.txt \
     /data/ig/hixchan_likes.txt \
@@ -14,6 +15,7 @@ python plot-all.py \
     /data/ig/v_count.txt \
     /data/ig/eva_count.txt \
     /data/ig/z_count.txt \
+    /data/ig/let_count.txt \
     /data/ig/newdone_count.txt \
     /data/ig/nesign_count.txt \
     /data/ig/hixchan_count.txt \
