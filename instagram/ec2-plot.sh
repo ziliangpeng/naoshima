115cfe8d463eb43bf07cd4b9aa782087f7bce762
Victor Peng <victor.peng@airbnb.com>
2018-01-16 20:47:58 -0800

    [ig] not plot for nesign or airbnb.office

--- instagram/ec2-plot.sh
+++ instagram/ec2-plot.sh
@@ -5,9 +5,9 @@ python plot-likes.py \
     /data/ig/eva_likes.txt \
     /data/ig/z_likes.txt \
     /data/ig/newdone_likes.txt \
-    /data/ig/nesign_likes.txt \
+#    /data/ig/nesign_likes.txt \
     /data/ig/hixchan_likes.txt \
-    /data/ig/airbnb_likes.txt \
+#    /data/ig/airbnb_likes.txt \
     /data/ig/mc_likes.txt \
     /data/ig/let_likes.txt
 
@@ -16,8 +16,8 @@ python plot-all.py \
     /data/ig/eva_count.txt \
     /data/ig/z_count.txt \
     /data/ig/newdone_count.txt \
-    /data/ig/nesign_count.txt \
+#    /data/ig/nesign_count.txt \
     /data/ig/hixchan_count.txt \
-    /data/ig/airbnb_count.txt \
+#    /data/ig/airbnb_count.txt \
     /data/ig/mc_count.txt \
     /data/ig/let_count.txt
