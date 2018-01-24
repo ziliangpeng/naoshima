b624f229b2d9b400c9261a99f3ef374e9f3e5a15
Victor Peng <victor.peng@airbnb.com>
2018-01-23 21:10:47 -0800

    [golang] gofmt

--- lang/golang/http.go
+++ lang/golang/http.go
@@ -1,24 +1,24 @@
 package main
 
 import (
-    "fmt"
-    "net/http"
-    "io/ioutil"
+	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    username := r.URL.Path[1:]
-    url := fmt.Sprintf("https://www.instagram.com/%s/?__a=1", username)
-    resp, _ := http.Get(url)
+	username := r.URL.Path[1:]
+	url := fmt.Sprintf("https://www.instagram.com/%s/?__a=1", username)
+	resp, _ := http.Get(url)
 
-    defer resp.Body.Close()
-    body, _ := ioutil.ReadAll(resp.Body)
+	defer resp.Body.Close()
+	body, _ := ioutil.ReadAll(resp.Body)
 
-    fmt.Printf(string(body))
-    fmt.Fprintf(w, string(body))
+	fmt.Printf(string(body))
+	fmt.Fprintf(w, string(body))
 }
 
 func main() {
-    http.HandleFunc("/", handler)
-    http.ListenAndServe(":8080", nil)
+	http.HandleFunc("/", handler)
+	http.ListenAndServe(":8080", nil)
 }
