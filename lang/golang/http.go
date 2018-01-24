c24f390f4bc011a74420641f7aea2a5d5d511dde
Victor Peng <victor.peng@airbnb.com>
2018-01-23 21:10:19 -0800

    [golang] http server and client (a proxy)

--- lang/golang/http.go
+++ lang/golang/http.go
@@ -3,10 +3,19 @@ package main
 import (
     "fmt"
     "net/http"
+    "io/ioutil"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "args: %s", r.URL.Path[1:])
+    username := r.URL.Path[1:]
+    url := fmt.Sprintf("https://www.instagram.com/%s/?__a=1", username)
+    resp, _ := http.Get(url)
+
+    defer resp.Body.Close()
+    body, _ := ioutil.ReadAll(resp.Body)
+
+    fmt.Printf(string(body))
+    fmt.Fprintf(w, string(body))
 }
 
 func main() {
