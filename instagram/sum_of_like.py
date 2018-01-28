7f8e94f1738331f8058d8a30f1fcc6b48a2aef29
Victor Peng <victor.peng@airbnb.com>
2018-01-27 23:42:02 -0800

    [ig] sum_of_like also report follow stats to statsd

--- instagram/sum_of_like.py
+++ instagram/sum_of_like.py
@@ -13,7 +13,13 @@ if r.status_code == 200:
     j = r.json()
     posts = _json_path(j, ["user", "media", "nodes"])
     likes = [_json_path(p, ["likes", "count"]) for p in posts]
-    statsd.gauge('naoshima.ig.likes', sum(likes), tags=["user:"+u])
+    statsd.gauge('naoshima.ig.likes', sum(likes), tags=["user:" + u])
     print(sum(likes))
+
+    follows = _json_path(j, ["user", "follows", "count"])
+    followed_by = _json_path(j, ["user", "followed_by", "count"])
+    statsd.gauge('naoshima.ig.stats', sum(likes), tags=["user:" + u, "type:likes"])
+    statsd.gauge('naoshima.ig.stats', follows, tags=["user:" + u, "type:follows"])
+    statsd.gauge('naoshima.ig.stats', followed_by, tags=["user:" + u, "type:followed_by"])
 else:
     pass
