b7f8c94fca4299d6231806f04b3b942a4e133d24
Victor Peng <victor.peng@airbnb.com>
2018-02-03 03:08:22 -0800

    [twitter] add logger

--- twitter-cli/unfollow_n.py
+++ twitter-cli/unfollow_n.py
@@ -2,12 +2,27 @@ import tweepy
 from auth import api
 import sys
 import itertools
+import logging
+
+
+formatter = logging.Formatter('[%(asctime)s] - %(levelname)s - [%(module)s:%(funcName)s:%(lineno)d] - %(message)s')
+
+sh = logging.StreamHandler()
+sh.setLevel(logging.INFO)
+sh.setFormatter(formatter)
+
+fh = logging.FileHandler('/data/twitter/logs/unfollow.log')
+fh.setLevel(logging.DEBUG)
+fh.setFormatter(formatter)
+
+logger = logging.getLogger('InstagramBot')
+logger.setLevel(logging.DEBUG)
+logger.addHandler(sh)
+logger.addHandler(fh)
 
 cnt = int(sys.argv[1])
-print('to unfollow %d users', cnt)
+logger.info('To unfollow %d users', cnt)
 
 for user in itertools.islice(tweepy.Cursor(api.friends).items(), cnt):  # lists all follows
-    print(user.screen_name)
-    print(user.name)
-    print(user.id)
+    logger.info("%d: %s(%s)", user.id, user.screen_name, user.name)
     api.destroy_friendship(user.id)
