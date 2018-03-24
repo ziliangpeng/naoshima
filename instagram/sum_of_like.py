a32d4b807f1415a9a349da6dd0857846ab497904
Victor Peng <victor.peng@airbnb.com>
2018-03-24 12:59:20 -0700

    [ig] fix sum_of_like (new graphql syntax)

--- instagram/sum_of_like.py
+++ instagram/sum_of_like.py
@@ -11,13 +11,13 @@ url = 'http://www.instagram.com/%s/?__a=1' % u
 r = requests.get(url)
 if r.status_code == 200:
     j = r.json()
-    posts = _json_path(j, ["user", "media", "nodes"])
-    likes = [_json_path(p, ["likes", "count"]) for p in posts]
+    posts = _json_path(j, ['graphql', "user", "edge_owner_to_timeline_media", "edges"])
+    likes = [_json_path(p, ["node", "edge_liked_by", "count"]) for p in posts]
     statsd.gauge('naoshima.ig.likes', sum(likes), tags=["user:" + u])
     print(sum(likes))
 
-    follows = _json_path(j, ["user", "follows", "count"])
-    followed_by = _json_path(j, ["user", "followed_by", "count"])
+    follows = _json_path(j, ['graphql', "user", "edge_follow", "count"])
+    followed_by = _json_path(j, ['graphql', "user", "edge_followed_by", "count"])
     statsd.gauge('naoshima.ig.stats', sum(likes), tags=["user:" + u, "type:likes"])
     statsd.gauge('naoshima.ig.stats', follows, tags=["user:" + u, "type:follows"])
     statsd.gauge('naoshima.ig.stats', followed_by, tags=["user:" + u, "type:followed_by"])
