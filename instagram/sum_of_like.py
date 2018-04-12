de87f50c9fe0e97b4b725197253730c2a2415f04
Victor Peng <victor.peng@airbnb.com>
2018-04-12 00:24:46 -0700

    [ig] sum_of_like.txt fix th way of getting likes count

--- instagram/sum_of_like.py
+++ instagram/sum_of_like.py
@@ -1,16 +1,18 @@
 import requests
 import sys
+import fetcher
 from user_utils import _json_path
 from datadog import statsd
 
 u = sys.argv[1]
 
 
-url = 'http://www.instagram.com/%s/?__a=1' % u
+# url = 'http://www.instagram.com/%s/?__a=1' % u
 
-r = requests.get(url)
-if r.status_code == 200:
-    j = r.json()
+# r = requests.get(url)
+status_code, j = fetcher.get_user_json(u)
+if status_code == 200:
+    # j = r.json()
     posts = _json_path(j, ['graphql', "user", "edge_owner_to_timeline_media", "edges"])
     likes = [_json_path(p, ["node", "edge_liked_by", "count"]) for p in posts]
     statsd.gauge('naoshima.ig.likes', sum(likes), tags=["user:" + u])
