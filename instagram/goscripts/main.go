14e4e944a3b75d7e84675ac397190e5b4da14483
Victor Peng <victor.peng@airbnb.com>
2018-05-05 13:02:18 -0700

    [ig] fix go scripts (reading follow and likes) for new IG protocol

--- instagram/goscripts/main.go
+++ instagram/goscripts/main.go
@@ -6,16 +6,56 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"reflect"
+	"strings"
 )
 
-func getMapKeyGeneral(m interface{}, keys ...string) interface{} {
+// works only for dict
+func getMapKeyGeneralDeprecated(m interface{}, keys ...string) interface{} {
 	for _, key := range keys {
 		m = m.(map[string]interface{})[key]
 	}
 	return m
 }
 
-func readJsonFromIg(username string) map[string]interface{} {
+// works better for json, support dict key reading and list index reading
+func getMapListKeyGeneral(m interface{}, keys ...interface{}) interface{} {
+	for _, key := range keys {
+		if reflect.TypeOf(key).String() == "string" {
+			m = m.(map[string]interface{})[key.(string)]
+		} else if reflect.TypeOf(key).String() == "int" {
+			m = m.([]interface{})[key.(int)]
+		}
+	}
+	return m
+}
+
+// IG changed protocol. read from HTML
+func readJsonFromIgHTML(username string) map[string]interface{} {
+	resp, err := http.Get(fmt.Sprintf("https://www.instagram.com/%s/", username))
+	defer resp.Body.Close()
+	if err != nil {
+	} // handle error
+	bodyBytes, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+	} // handle error
+
+	bodyString := string(bodyBytes)
+	// fmt.Println(bodyString)
+	lines := strings.Split(bodyString, "\n")
+	line := lines[208]
+	// fmt.Println(line)
+	startIndex := strings.Index(line, "{")
+	endIndex := strings.LastIndex(line, "}") + 1
+	j := line[startIndex:endIndex]
+
+	var dat map[string]interface{}
+	json.Unmarshal([]byte(j), &dat)
+	return dat
+}
+
+// old IG protocol. Not work any more.
+func readJsonFromIgDeprecated(username string) map[string]interface{} {
 	resp, err := http.Get(fmt.Sprintf("https://www.instagram.com/%s/?__a=1", username))
 	defer resp.Body.Close()
 	if err != nil {
@@ -30,19 +70,21 @@ func readJsonFromIg(username string) map[string]interface{} {
 }
 
 func readFollowStats(username string) {
-	dat := readJsonFromIg(username)
-	followedBy := getMapKeyGeneral(dat, "graphql", "user", "edge_followed_by", "count").(float64)
-	follow := getMapKeyGeneral(dat, "graphql", "user", "edge_follow", "count").(float64)
+	dat := readJsonFromIgHTML(username)
+	datJSON := getMapListKeyGeneral(dat, "entry_data", "ProfilePage", 0)
+	followedBy := getMapListKeyGeneral(datJSON, "graphql", "user", "edge_followed_by", "count").(float64)
+	follow := getMapListKeyGeneral(datJSON, "graphql", "user", "edge_follow", "count").(float64)
 
 	fmt.Println(int64(followedBy), int64(follow))
 }
 
 func readLikesStats(username string) {
-	dat := readJsonFromIg(username)
-	media := getMapKeyGeneral(dat, "graphql", "user", "edge_owner_to_timeline_media", "edges").([]interface{})
+	dat := readJsonFromIgHTML(username)
+	datJSON := getMapListKeyGeneral(dat, "entry_data", "ProfilePage", 0)
+	media := getMapListKeyGeneral(datJSON, "graphql", "user", "edge_owner_to_timeline_media", "edges").([]interface{})
 	likes := int64(0)
 	for _, entry := range media {
-		likes += int64(getMapKeyGeneral(entry.(map[string]interface{}), "node", "edge_liked_by", "count").(float64))
+		likes += int64(getMapListKeyGeneral(entry.(map[string]interface{}), "node", "edge_liked_by", "count").(float64))
 	}
 	fmt.Println(likes)
 }
