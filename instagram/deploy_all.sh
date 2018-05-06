7785c96a969753473983976a0e84230fcd3fc14a
Victor Peng <victor.peng@airbnb.com>
2018-05-05 21:41:36 -0700

    [ig] sleep 600 in deploy_all

--- instagram/deploy_all.sh
+++ instagram/deploy_all.sh
@@ -1,9 +1,15 @@
 #!/usr/bin/env bash
 
 ./deploy.sh v
+sleep 600
 ./deploy.sh z
+sleep 600
 ./deploy.sh eva
+sleep 600
 ./deploy.sh mc
+sleep 600
 ./deploy.sh hix
+sleep 600
 ./deploy.sh eaxy
+sleep 600
 ./deploy.sh newdone
