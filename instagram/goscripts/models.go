ef5fb8e0e98fd65d94a4d9fc45a6848e9756f717
Victor Peng <victor.peng@airbnb.com>
2018-05-06 02:02:13 -0700

    [ig] better models in go

--- instagram/goscripts/models.go
+++ instagram/goscripts/models.go
@@ -6,20 +6,58 @@ import (
   "io/ioutil"
 )
 
-// User: Instagram User struct
+type Media struct {
+  ID string `json:"id"`
+  Code string `json:"shortcode"`
+  Type string `json:"__typename"`
+  Dimentions struct {
+    Height int `json:"height"`
+    Width int `json:"width"`
+  } `json:"dimensions"`
+  Timestamp int `json:"taken_at_timestamp"`
+  LikedBy struct {
+    Count int `json:"count"`
+  } `json:"edge_liked_by"`
+}
+
 type User struct {
+  ID string `json:"id"`
+  Name string `json:"full_name"`
+  Username string `json:"username"`
   Biography string `json:"biography"`
+  IsPrivate bool `json:"is_private"`
+  IsVerified bool `json:"is_verified"`
+
+  FollowedBy struct {
+    Count int `json:"count"`
+  } `json:"edge_followed_by"`
+
+  Follows struct {
+    Count int `json:"count"`
+  } `json:"edge_follow"`
+
+  Medias struct {
+    Count int `json:"count"`
+    MediaEdges []struct {
+      Media Media `json:"node"`
+    } `json:"edges"`
+  } `json:"edge_owner_to_timeline_media"`
 }
 
 func test() {
   filename := "ig_user_data.json"
   bytes, _ := ioutil.ReadFile(filename)
-  var dat map[string]interface{}
-  json.Unmarshal(bytes, &dat)
-  datUser := getMapListKeyGeneral(dat, "graphql", "user")
-  var u User
-  userBytes, _ := json.Marshal(datUser)
-  json.Unmarshal([]byte(userBytes), &u)
-  fmt.Println(u)
-  fmt.Println(u.Biography)
+
+  var data struct {
+    GraphQL struct {
+      User User `json:"user"`
+    } `json:"graphql"`
+  }
+
+  json.Unmarshal([]byte(bytes), &data)
+
+  user := data.GraphQL.User
+  fmt.Printf("%+v\n\n", user)
+  j, _ := json.Marshal(user)
+  fmt.Println(string(j))
 }
