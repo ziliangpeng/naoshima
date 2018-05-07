71d6367c87c9ee9b8f11d875ff19a7c2127d3ba2
Victor Peng <victor.peng@airbnb.com>
2018-05-06 21:06:04 -0700

    [ig][grpc] python client takes sys.argv[1] as username

--- instagram/goscripts/python_client.py
+++ instagram/goscripts/python_client.py
@@ -2,11 +2,15 @@ import ig_pb2_grpc
 import ig_pb2
 import grpc
 
+import sys
+
 
 host = 'localhost'
 port = 8888
 channel = grpc.insecure_channel('%s:%d' % (host, port))
 stub = ig_pb2_grpc.InstagramStub(channel)
 
-user = stub.GetUser(ig_pb2.GetUserRequest(username="aaa"))
+get_user_request = ig_pb2.GetUserRequest()
+get_user_request.username = sys.argv[1]
+user = stub.GetUser(get_user_request)
 print(user)
