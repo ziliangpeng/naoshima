6f6a2169a95f8a87d78d3d62cfdd217a197468c5
Victor Peng <victor.peng@airbnb.com>
2018-05-20 13:55:42 -0700

    [ig] make sure dd use host in DD_HOST

--- instagram/dd.py
+++ instagram/dd.py
@@ -6,10 +6,10 @@ DD_HOST_KEY = 'DD_HOST'
 
 if DD_HOST_KEY in os.environ:
     dd_host = os.getenv(DD_HOST_KEY)
-    datadog.initialize(host_name=dd_host)
+    statsd = datadog.DogStatsd(host=dd_host)
     logger.info("Datadog host is %s", dd_host)
 else:
     logger.info("User default Datadog host")
+    statsd = datadog.statsd
 
-
-statsd = datadog.statsd
+logger.info("dd host is actually %s", statsd.host)
