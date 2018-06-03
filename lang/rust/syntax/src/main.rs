1b3faf75c9a0271b8bd1cbe0b259e346b9849ea0
Victor Peng <victor.peng@airbnb.com>
2018-06-02 19:15:08 -0700

    [rust] some simple syntax example

--- lang/rust/syntax/src/main.rs
+++ lang/rust/syntax/src/main.rs
@@ -1,3 +1,33 @@
+fn f(mut s1: String, s2: &mut String) -> String {
+  s1.push_str("[mut s1]");
+  s2.push_str("[mut s2]");
+  let mut s3 = String::from(s1.as_str());
+  s3.push_str(s2.as_str());
+  s3 // &s3 will fail. s3 will be out of scope befor moving ownership out.
+}
+
 fn main() {
-    println!("Hello, world!");
+  let hello = String::from("Hello, V!");
+  println!("{}", hello);
+
+  let mut count = 0;  // Mutable need to use `mut`
+  println!("Count is {}", count);
+  count += 1;
+  println!("Count is {}", count);
+  let count = "NaN";  // Shadowing. Redeclare the variable.
+  println!("Count is {}", count);
+
+
+  let s1 = String::from("s1");
+  let mut s2 = String::from("s2");
+  let s3 = f(s1, &mut s2);
+  println!("s3 is {}", s3);
+
+  // s1.push_str("!"); will fail. s1 ownership is moved
+  s2.push_str("!");  // fine. s2 ownership not moved.
+
+
+  let s4 = s3; // move s3 ownership to s4. s3 not usable now.
+  // println!("s3 is {}", s3); will fail
+  println!("s4 is {} (should equals s3)", s4);
 }
