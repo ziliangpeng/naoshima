6f644ad891e2d16eb78097b1e54b0b97f7379e24
Victor Peng <victor.peng@airbnb.com>
2018-06-22 20:09:24 -0700

    [setup] fix typo in install-kube

--- setup/install-kubectl.sh
+++ setup/install-kubectl.sh
@@ -5,4 +5,4 @@ curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add
 sudo touch /etc/apt/sources.list.d/kubernetes.list
 echo "deb http://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee -a /etc/apt/sources.list.d/kubernetes.list
 sudo apt-get update
-sudp apt-get install -y kubelet kubeadm kubectl
+sudo apt-get install -y kubelet kubeadm kubectl
