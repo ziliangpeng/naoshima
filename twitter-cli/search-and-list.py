f8a2ae4c2a74f500bcc5c6a8d47750015d360510
Victor Peng <victor.peng@airbnb.com>
2018-07-18 15:26:44 -0700

    [twitter] print tweet when searching

--- twitter-cli/search-and-list.py
+++ twitter-cli/search-and-list.py
@@ -9,5 +9,8 @@ list_id = sys.argv[2]
 
 results = api.search(q=keyword, lang='zh')
 for r in results:
+    print(r.text)
     u = r.user
+    print(u.name)
+    print('+' * 42)
     api.add_list_member(user_id=u.id, list_id=list_id)
