bbbbd84b17dc544b7503d43ab632f46201fb6612
Victor Peng <victor.peng@airbnb.com>
2018-07-17 21:42:14 -0700

    [crypt] time the crypt

--- crypt/main.py
+++ crypt/main.py
@@ -1,5 +1,19 @@
 import rsa
 import aes
+from timeit import default_timer as timer
 
-rsa.run()
-aes.run()
+start_1 = timer()
+for i in range(100):
+    rsa.run()
+end_1 = timer()
+elapsed_1 = end_1 - start_1
+
+start_2 = timer()
+for i in range(100):
+    aes.run()
+end_2 = timer()
+elapsed_2 = end_2 - start_2
+
+
+print('RSA', elapsed_1)
+print('AES', elapsed_2)
