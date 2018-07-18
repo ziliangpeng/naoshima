ca21856b46795d5c0a17d936208d2ee1a8953b0e
Victor Peng <victor.peng@airbnb.com>
2018-07-17 22:08:10 -0700

    [crypt] use multiple repeats

--- crypt/main.py
+++ crypt/main.py
@@ -4,22 +4,23 @@ import aes
 from time import time
 
 if len(sys.argv) > 1:
-    repeat = int(sys.argv[1])
+    repeat = map(int, sys.argv[1:])
 else:
-    repeat = 1
+    repeat = [1]
 
-start_1 = time()
-for i in range(repeat):
-    rsa.run()
-end_1 = time()
-elapsed_1 = end_1 - start_1
+for r in repeat:
+    start_1 = time()
+    for i in range(r):
+        rsa.run()
+    end_1 = time()
+    elapsed_1 = end_1 - start_1
 
-start_2 = time()
-for i in range(repeat):
-    aes.run()
-end_2 = time()
-elapsed_2 = end_2 - start_2
+    start_2 = time()
+    for i in range(r):
+        aes.run()
+    end_2 = time()
+    elapsed_2 = end_2 - start_2
 
-print("Repeat:", repeat)
-print('RSA', elapsed_1)
-print('AES', elapsed_2)
+    print("Repeat:", r)
+    print('RSA', elapsed_1)
+    print('AES', elapsed_2)
