133d3794d45879997fadc7b524d70949950302a4
Victor Peng <victor.peng@airbnb.com>
2019-02-09 22:30:25 -0800

    [img] play with it

--- image-processing/test.py
+++ image-processing/test.py
@@ -9,11 +9,13 @@ plt.rcParams['image.interpolation'] = 'none'
 
 ###
 
+import skimage
 from skimage import data, filters, io
 
 image = data.camera()
 fname = 'chelmico.jpg'
 image = io.imread(fname)
+image = skimage.color.rgb2grey(image)
 # Ignore the Gaussian filter, for now.
 # (This is explained at the end of the article.)
 smooth_image = filters.gaussian(image, 5)
@@ -30,5 +32,14 @@ horizontal_edge_kernel = np.array([[ 1,  2,  1],
                                    [-1, -2, -1]])
 # Use non-gray colormap to display negative values as red and positive
 # values as blue.
-plt.imshow(horizontal_edge_kernel, cmap=plt.cm.RdBu)
+# plt.imshow(horizontal_edge_kernel, cmap=plt.cm.RdBu)
+# plt.show()
+
+
+###
+
+from scipy.ndimage import convolve
+
+horizontal_edge_response = convolve(smooth_image, horizontal_edge_kernel)
+plt.imshow(horizontal_edge_response, cmap=plt.cm.RdBu);
 plt.show()
