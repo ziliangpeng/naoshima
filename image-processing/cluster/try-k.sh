9dd80aea3210e2b0441316882ba6cad533e7466b
Victor Peng <zpeng@nuro.ai>
2019-05-12 20:17:28 -0700

    less iteration

--- image-processing/cluster/try-k.sh
+++ image-processing/cluster/try-k.sh
@@ -6,6 +6,6 @@ python cluster.py -filename $1 -k 4
 python cluster.py -filename $1 -k 5
 python cluster.py -filename $1 -k 6
 python cluster.py -filename $1 -k 7
-python cluster.py -filename $1 -k 8
-python cluster.py -filename $1 -k 9
+#python cluster.py -filename $1 -k 8
+#python cluster.py -filename $1 -k 9
  
