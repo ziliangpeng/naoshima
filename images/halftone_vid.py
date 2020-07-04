4538b8e9c80595b1f5cfede76c0fc8d7a2cd1ae1
Victor Peng <ziliangdotme@gmail.com>
2020-07-04 13:16:06 -0700

    refactor halftone_vid.py

--- images/halftone_vid.py
+++ images/halftone_vid.py
@@ -4,29 +4,23 @@ import os
 import cv2
 from PIL import Image, ImageDraw, ImageStat
 import numpy
+import datetime
+import glog
 
 path = sys.argv[1]
 
 
-def animate(file, vid, del1, del2, del3, del4, steps):
+def animate(im, vid, del1, del2, del3, del4, steps):
     for i in range(steps):
-        h = Halftone(file)
+        glog.info('Iter %d' % (i))
+        h = Halftone("")
 
-        sample=10
-        scale=1
-        percentage=0
-        # filename_addition="_halftoned"
-        # angles=[0, 1, 3, 4]
-        angles=[del1 * i, del2 * i, del3 * i, del4 * i]
-        style="color"
-        antialias=False
-
-        f, e = os.path.splitext(path)
-
-        try:
-            im = Image.open(path)
-        except IOError:
-            raise
+        sample = 10
+        scale = 1
+        percentage = 0
+        angles = [del1 * i, del2 * i, del3 * i, del4 * i]
+        style = "color"
+        antialias = False
 
         if style == "grayscale":
             angles = angles[:1]
@@ -41,18 +35,29 @@ def animate(file, vid, del1, del2, del3, del4, steps):
         imtemp = new.copy().convert('RGB')
         vid.write(cv2.cvtColor(numpy.array(imtemp), cv2.COLOR_RGB2BGR))
 
-frame_width = 600 #new.width
-frame_height = 600 # new.height
-fourcc = cv2.VideoWriter_fourcc(*'X264')
-out = cv2.VideoWriter('outpy.mkv',fourcc, 10, (frame_width,frame_height))
+
+f, e = os.path.splitext(path)
+
+try:
+    im = Image.open(path)
+except IOError:
+    raise
+
+frame_width = im.width
+frame_height = im.height
+glog.info('size: %d, %d' % (frame_width, frame_height))
+fourcc = cv2.VideoWriter_fourcc(*'mp4v')
+out = cv2.VideoWriter(
+    'out_%d.mp4' % (int(datetime.datetime.utcnow().timestamp())), fourcc, 10,
+    (frame_width, frame_height))
 
 # animate(path, out, 1, 0, 0, 0, 90)
 # animate(path, out, 0, 1, 0, 0, 90)
 # animate(path, out, 0, 0, 1, 0, 90)
 # animate(path, out, 0, 0, 0, 1, 90)
 
-animate(path, out, 1, 2, 3, 4, 90)
+animate(im, out, 1, 2, 3, 4, 90)
 
 out.release()
 
-cv2.destroyAllWindows() 
+cv2.destroyAllWindows()
