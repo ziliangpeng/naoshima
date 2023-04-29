da2839303e887b94b704306811bb1eae9dfded40
Victor <ziliangdotme@gmail.com>
2023-04-28 19:00:10 -0700

    add some ml code

---
+++ dnn/dnn.py
@@ -0,0 +1,66 @@
+import tensorflow as tf
+from tensorflow.keras.datasets import mnist
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.layers import Dense, Flatten
+from tensorflow.keras.optimizers import Adam
+from tensorflow.keras.losses import SparseCategoricalCrossentropy
+import matplotlib.pyplot as plt
+import numpy as np
+
+# Load the MNIST dataset
+(x_train, y_train), (x_test, y_test) = mnist.load_data()
+
+# Normalize the data
+x_train, x_test = x_train / 255.0, x_test / 255.0
+
+# Create a simple DNN model
+model = Sequential([
+    Flatten(input_shape=(28, 28)),
+    Dense(128, activation='relu'),
+    Dense(16, activation='relu'),
+    Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer=Adam(), loss=SparseCategoricalCrossentropy(), metrics=['accuracy'])
+
+# Train the model using CPU
+with tf.device('/GPU:0'):
+    history = model.fit(x_train, y_train, epochs=10, batch_size=32, validation_split=0.2)
+
+# Evaluate the model on the test set
+loss, accuracy = model.evaluate(x_test, y_test)
+print(f'Test loss: {loss:.4f}')
+print(f'Test accuracy: {accuracy:.4f}')
+
+# # Plot the training loss and accuracy
+# plt.figure(figsize=(12, 4))
+# plt.subplot(1, 2, 1)
+# plt.plot(history.history['loss'], label='Training Loss')
+# plt.plot(history.history['val_loss'], label='Validation Loss')
+# plt.xlabel('Epoch')
+# plt.ylabel('Loss')
+# plt.legend()
+
+# plt.subplot(1, 2, 2)
+# plt.plot(history.history['accuracy'], label='Training Accuracy')
+# plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
+# plt.xlabel('Epoch')
+# plt.ylabel('Accuracy')
+# plt.legend()
+
+# plt.show()
+
+# # Visualize example predictions
+# num_examples = 10
+# predictions = model.predict(x_test[:num_examples])
+
+# plt.figure(figsize=(10, 5))
+# for i in range(num_examples):
+#     plt.subplot(2, 5, i+1)
+#     plt.imshow(x_test[i], cmap='gray')
+#     plt.title(f"True: {y_test[i]}\nPred: {np.argmax(predictions[i])}")
+#     plt.axis('off')
+
+# plt.tight_layout()
+# plt.show()
---
+++ dnn/linear-regression.py
@@ -0,0 +1,34 @@
+import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import mean_squared_error, r2_score
+
+# Generate a synthetic dataset
+np.random.seed(42)
+X = np.random.rand(100, 1)
+y = 2 + 3 * X + np.random.randn(100, 1)
+
+# Split the data into training and testing sets
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+# Create a linear regression model and fit it to the training data
+model = LinearRegression()
+model.fit(X_train, y_train)
+
+# Make predictions on the testing set
+y_pred = model.predict(X_test)
+
+# Evaluate the model
+mse = mean_squared_error(y_test, y_pred)
+r2 = r2_score(y_test, y_pred)
+print(f"Mean Squared Error: {mse:.2f}")
+print(f"R-squared: {r2:.2f}")
+
+# Plot the results
+plt.scatter(X_test, y_test, color='blue', label='Actual')
+plt.scatter(X_test, y_pred, color='red', label='Predicted')
+plt.xlabel('X')
+plt.ylabel('y')
+plt.legend()
+plt.show()
