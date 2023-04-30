a8ceb0ca0b91781338d02dfe1d9bc1a4baf0d216
Victor <ziliangdotme@gmail.com>
2023-04-30 05:13:06 -0700

    completes linear regression in gradient descent. use video to visualize.

--- ml/dnn/linear-regression-gradient-descent.py
+++ ml/dnn/linear-regression-gradient-descent.py
@@ -1,4 +1,6 @@
 import numpy as np
+import matplotlib.pyplot as plt
+import imageio
 
 def compute_cost(X, y, theta):
     m = len(y)
@@ -9,37 +11,77 @@ def compute_cost(X, y, theta):
 def gradient_descent(X, y, theta, learning_rate, num_iterations):
     m = len(y)
     cost_history = np.zeros(num_iterations)
+    theta_history = []
     
     for i in range(num_iterations):
         predictions = X.dot(theta)
+        # So here's how I understand it:
+        # X is a Nx2 matrix, first colum all 1, second column all x.
+        # theta is 2x1 (intercept, slope)
+        #   theta is a matrix that represents 2 parameters.
+        # y is Nx1 matrix.
+        # X.dot(theta) gets a 2x1 predictions, basically (1 * theta[0], x * theta[1])
+        #
+        # so this is optimizing 2 parameters at the same time.
+        # - sum of y diff minimize - find intercept
+        # - weighted y diff minimize - find slope
+        #
+        # in DNN world, the `theta -=` part is the back propagation, basically updating weights.
+        # there is no activation function, since there's no need for non-linearity. (or maybe I can understand it as there's a naive activation function?)
         theta -= (1/m) * learning_rate * (X.T.dot(predictions-y))
+        theta_history.append(theta.copy())
+        # if i == 1 or i == num_iterations - 1:
+            # print("inspecting theta")
+            # print(X)
+            # print(y)
+            # print(X.T.dot(predictions-y))
+            # print((1/m) * learning_rate * (X.T.dot(predictions-y)))
+            # print(theta)
+            # print("done inspecting theta")
+        if i % 1000 == 0:
+            print(theta)
         cost_history[i] = compute_cost(X, y, theta)
         
-    return theta, cost_history
+    return theta, cost_history, theta_history
 
-# Load your data
-# X, y = load_your_data()
 # Generate random data
-num_points = 100
+num_points = 1000
 np.random.seed(42)
-X = np.random.rand(num_points, 1)
+x = np.random.rand(num_points, 1)
 slope = 2
 intercept = 3
 noise = np.random.randn(num_points, 1) * 0.1
-y = slope * X + intercept + noise
+y = slope * x + intercept + noise
 
 # Add the bias term (column of ones) to the X matrix
-X = np.hstack((np.ones((X.shape[0], 1)), X))
+X = np.hstack((np.ones((x.shape[0], 1)), x))
 
 # Initialize the parameters (theta)
-theta = np.zeros(X.shape[1])
+theta = np.zeros((X.shape[1], 1))
 
 # Set the hyperparameters
-learning_rate = 0.01
-num_iterations = 1000
+# NOTE: yeah, so a learning of 2 messes up the learning lol
+learning_rate = 0.1
+num_iterations = 200
 
 # Perform gradient descent
-theta, cost_history = gradient_descent(X, y, theta, learning_rate, num_iterations)
+theta, cost_history, theta_history = gradient_descent(X, y, theta, learning_rate, num_iterations)
 
 # Print the optimized theta values
+print("slope: %d, intercept %d" % (slope, intercept))
 print("Optimized theta:", theta)
+
+
+# print(theta_history)
+for i, t in enumerate(theta_history):
+    plt.scatter(x, y)
+    plt.plot(x, X.dot(t), color='red')
+    plt.xlabel("x")
+    plt.ylabel("y")
+    plt.title(f"Iteration {i+1}")
+    plt.savefig(f"lrgs/iteration_{i+1}.png")
+    plt.clf()
+with imageio.get_writer('lrgs/gradient_descent_optimization.mp4', mode='I', fps=32) as writer:
+    for i in range(num_iterations):
+        image = imageio.imread(f"lrgs/iteration_{i+1}.png")
+        writer.append_data(image)
--- ml/dnn/linear-regression-in-dnn.py
+++ ml/dnn/linear-regression-in-dnn.py
@@ -4,7 +4,7 @@ import numpy as np
 # Randomized sample data
 np.random.seed(42)
 X = np.random.rand(1000, 1) * 5
-y = 3 * X + 1 + np.random.normal(0, 0.5, size=(1000, 1))
+y = 3 * X + 2 + np.random.normal(0, 0.5, size=(1000, 1))
 
 # Define the neural network model (one-layer DNN)
 model = tf.keras.Sequential([
@@ -17,9 +17,12 @@ model.compile(optimizer=tf.keras.optimizers.SGD(learning_rate=0.01),
               metrics=['accuracy'])
 
 # Train the model
+# Is there a way to visualize how the weights changed?
 model.fit(X, y, epochs=100, verbose=1)
 
 # Print the learned weights and bias
 weights, bias = model.layers[0].get_weights()
 print("Learned weight:", weights[0][0])
 print("Learned bias:", bias[0])
+print(weights)
+print(bias)
