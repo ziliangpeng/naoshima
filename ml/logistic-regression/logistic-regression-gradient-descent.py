04a6d01b61b9fd5d656ed50af47ac71410a2f75f
Victor <ziliangdotme@gmail.com>
2023-05-03 17:39:49 -0700

    visualize

--- ml/logistic-regression/logistic-regression-gradient-descent.py
+++ ml/logistic-regression/logistic-regression-gradient-descent.py
@@ -1,11 +1,14 @@
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_breast_cancer
 from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import StandardScaler
 from sklearn.metrics import accuracy_score
 
+
 def sigmoid(x):
     return 1 / (1 + np.exp(-x))
+
 
 def logistic_regression(X, y, num_iterations, learning_rate):
     num_features = X.shape[1]
@@ -24,11 +27,14 @@ def logistic_regression(X, y, num_iterations, learning_rate):
 
     return weights, bias
 
+
 data = load_breast_cancer()
-X = data.data
+X = data.data[:, :2]  # Select the first two features for visualization
 y = data.target[:, np.newaxis]
 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
 
 scaler = StandardScaler()
 X_train = scaler.fit_transform(X_train)
@@ -44,3 +50,26 @@ y_test_pred = (y_test_pred > 0.5).astype(int)
 
 accuracy = accuracy_score(y_test, y_test_pred)
 print(f"Accuracy: {accuracy}")
+
+# Visualization
+plt.scatter(
+    X_train[y_train.ravel() == 0, 0],
+    X_train[y_train.ravel() == 0, 1],
+    label="Class 0",
+    alpha=0.5,
+)
+plt.scatter(
+    X_train[y_train.ravel() == 1, 0],
+    X_train[y_train.ravel() == 1, 1],
+    label="Class 1",
+    alpha=0.5,
+)
+
+x_values = np.linspace(X_train[:, 0].min() - 1, X_train[:, 0].max() + 1, 100)
+y_values = -(bias + weights[0] * x_values) / weights[1]
+
+plt.plot(x_values, y_values, label="Decision Boundary")
+plt.xlabel("Feature 1")
+plt.ylabel("Feature 2")
+plt.legend()
+plt.show()
