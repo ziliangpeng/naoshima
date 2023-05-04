044862baee56285e5db864b0b98af2c837b20c30
Victor <ziliangdotme@gmail.com>
2023-05-04 13:10:43 -0700

    CNN for CIFAR-10

---
+++ ml/CIFAR-10/cifar10-cnn.py
@@ -0,0 +1,41 @@
+import tensorflow as tf
+from tensorflow.keras import datasets, layers, models
+import os
+import datetime
+
+# Load the CIFAR-10 dataset
+(train_images, train_labels), (test_images, test_labels) = datasets.cifar10.load_data()
+
+# Normalize the pixel values
+train_images, test_images = train_images / 255.0, test_images / 255.0
+
+# Create the CNN model
+model = models.Sequential([
+    layers.Conv2D(32, (3, 3), activation='relu', input_shape=(32, 32, 3)),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.MaxPooling2D((2, 2)),
+    layers.Conv2D(64, (3, 3), activation='relu'),
+    layers.Flatten(),
+    layers.Dense(64, activation='relu'),
+    layers.Dense(10, activation='softmax')
+])
+
+# Compile the model
+model.compile(optimizer='adam',
+              loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
+              metrics=['accuracy'])
+
+log_dir = os.path.join("logs", 'cnn-' + datetime.datetime.now().strftime("%Y%m%d-%H%M%S"))
+
+# Create the TensorBoard callback
+tensorboard_callback = tf.keras.callbacks.TensorBoard(
+    log_dir=log_dir, histogram_freq=1, update_freq='epoch')
+
+# Train the model
+history = model.fit(train_images, train_labels, epochs=10,
+                    validation_data=(test_images, test_labels), callbacks=[tensorboard_callback])
+
+# Evaluate the model on the test set
+test_loss, test_accuracy = model.evaluate(test_images, test_labels)
+print(f'Test accuracy: {test_accuracy:.2f}')
