12a2c8557bfd90c44fefecd8f5168c9a8c266264
Ziliang Peng <ziliangdotme@gmail.com>
2023-08-13 20:29:12 -0700

    fix mnist

--- ml/cnn/mnist-cnn.py
+++ ml/cnn/mnist-cnn.py
@@ -6,7 +6,10 @@ from tensorflow.keras import datasets, layers, models
 (train_images, train_labels), (test_images, test_labels) = datasets.mnist.load_data()
 
 # # Optionally, drop in Fashion MNIST to replace MNIST
-(train_images, train_labels), (test_images, test_labels) = datasets.fashion_mnist.load_data()
+(train_images, train_labels), (
+    test_images,
+    test_labels,
+) = datasets.fashion_mnist.load_data()
 
 # Normalize the images
 train_images = train_images / 255.0
@@ -16,13 +19,14 @@ test_images = test_images / 255.0
 train_images = train_images.reshape(-1, 28, 28, 1)
 test_images = test_images.reshape(-1, 28, 28, 1)
 
+
 def model1():
     return models.Sequential(
         [
             layers.Conv2D(32, (3, 3), activation="relu", input_shape=(28, 28, 1)),
             layers.MaxPooling2D((2, 2)),
-            # layers.Conv2D(64, (3, 3), activation="relu"),
-            # layers.MaxPooling2D((2, 2)),
+            layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.MaxPooling2D((2, 2)),
             layers.Conv2D(64, (3, 3), activation="relu"),
             layers.MaxPooling2D((2, 2)),
             layers.Conv2D(64, (3, 3), activation="relu"),
@@ -32,21 +36,19 @@ def model1():
         ]
     )
 
+
 def model2():
     return models.Sequential(
+        # Fully working NN with ~90% accuracy
         [
-            # layers.Conv2D(4, (3, 3), activation="relu", input_shape=(28, 28, 1)),
-            # layers.MaxPooling2D((2, 2)),
-            # layers.Conv2D(64, (3, 3), activation="relu"),
-            # layers.MaxPooling2D((2, 2)),
-            # layers.Conv2D(64, (3, 3), activation="relu"),
+            layers.Conv2D(4, (3, 3), activation="relu", input_shape=(28, 28, 1)),
             layers.Flatten(),
-            # layers.Dense(32, activation="relu"),
             layers.Dense(10, activation="softmax"),
         ]
     )
 
-model = model1()
+
+model = model2()
 
 # Compile the model
 model.compile(
@@ -55,7 +57,7 @@ model.compile(
 
 # Train the model
 history = model.fit(
-    train_images, train_labels, epochs=500, batch_size=1024, validation_split=0.2
+    train_images, train_labels, epochs=20, batch_size=128, validation_split=0.2
 )
 
 # Evaluate the model on the test set
