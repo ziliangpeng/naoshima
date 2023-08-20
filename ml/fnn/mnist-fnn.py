d12ff843a6b03fcd194b4a5c8d2b480bc6bf4736
Ziliang Peng <ziliangdotme@gmail.com>
2023-08-19 21:34:07 -0700

    delete plot code for mnist fnn

--- ml/fnn/mnist-fnn.py
+++ ml/fnn/mnist-fnn.py
@@ -19,7 +19,7 @@ model = Sequential(
     [
         Flatten(input_shape=(28, 28)),
         Dense(128, activation="relu"),
-        Dense(16, activation="relu"),
+        Dense(64, activation="relu"),
         Dense(10, activation="softmax"),
     ]
 )
@@ -42,41 +42,8 @@ print(f"Test accuracy: {accuracy:.4f}")
 
 # print weights
 for l in model.layers:
-    # print("layer:", l)
     if not l.get_weights():
         continue
     weights, bias = l.get_weights()
     print("Learned weight:", weights[0][0])
     print("Learned bias:", bias[0])
-
-# # Plot the training loss and accuracy
-# plt.figure(figsize=(12, 4))
-# plt.subplot(1, 2, 1)
-# plt.plot(history.history['loss'], label='Training Loss')
-# plt.plot(history.history['val_loss'], label='Validation Loss')
-# plt.xlabel('Epoch')
-# plt.ylabel('Loss')
-# plt.legend()
-
-# plt.subplot(1, 2, 2)
-# plt.plot(history.history['accuracy'], label='Training Accuracy')
-# plt.plot(history.history['val_accuracy'], label='Validation Accuracy')
-# plt.xlabel('Epoch')
-# plt.ylabel('Accuracy')
-# plt.legend()
-
-# plt.show()
-
-# # Visualize example predictions
-# num_examples = 10
-# predictions = model.predict(x_test[:num_examples])
-
-# plt.figure(figsize=(10, 5))
-# for i in range(num_examples):
-#     plt.subplot(2, 5, i+1)
-#     plt.imshow(x_test[i], cmap='gray')
-#     plt.title(f"True: {y_test[i]}\nPred: {np.argmax(predictions[i])}")
-#     plt.axis('off')
-
-# plt.tight_layout()
-# plt.show()
