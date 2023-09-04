787d9c781fb71b682ca84f4c8ea2479b30cdc209
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-04 15:57:32 -0700

    extract nurochat into class and format

--- ml/llm/nurochat/nuro-chat.py
+++ ml/llm/nurochat/nuro-chat.py
@@ -16,84 +16,106 @@ Others:
 text/html; charset=utf-8
 """
 
-def crawl_website(url):
-    # Send a GET request to the URL
-    response = requests.get(url)
 
-    mime_type = response.headers.get('content-type')
-    if not mime_type.startswith('text/html'):
-        return "MIME type: {}".format(mime_type), []
-    else:
-        print("MIME type: {}".format(mime_type))
-    
-    # Get the response code
-    response_code = response.status_code
-    if response_code != 200:
-        return "RESPONSE CODE IS {} for {}".format(response_code, url), []
-    
+class Crawler:
+    def __init__(self, seed, interest_prefix=None):
+        self.seed = seed
+        if interest_prefix is None:
+            interest_prefix = [seed]
+        self.interest_prefix = interest_prefix
+        self.external_links = set()
 
-    # Parse the HTML content using BeautifulSoup
-    soup = BeautifulSoup(response.content, 'html.parser')
+        self.queue = [seed]
+        self.archive = {}
+        self.external_links = set()
 
-    # Extract all text from the HTML content
-    text = soup.get_text()
-    links = [link.get('href') for link in soup.find_all('a')]
+    def _is_interesting(self, url):
+        for prefix in self.interest_prefix:
+            if url.startswith(prefix):
+                return True
+        return False
 
-    return text, links
+    def _sanitize_url(self, url):
+        return url.split("?")[0]
 
-INTEREST_PREFIX = ['https://medium.com/nuro', 'https://nuro.ai', 'https://www.nuro.ai', 'http://nuro.ai', 'http://www.nuro.ai']
+    def crawl(self):
+        while self.queue:
+            url = self.queue.pop()
+            self.crawl_one_and_expand(url)
 
-def is_interesting(url):
-    for prefix in INTEREST_PREFIX:
-        if url.startswith(prefix):
-            return True
-    return False
+        for el in sorted(self.external_links):
+            print(el)
 
-def sanitize_url(url):
-    return url.split('?')[0]
+    def crawl_website(self, url):
+        # Send a GET request to the URL
+        response = requests.get(url)
 
-SEED = 'https://nuro.ai'
+        mime_type = response.headers.get("content-type")
+        if not mime_type.startswith("text/html"):
+            return "MIME type: {}".format(mime_type), []
+        else:
+            print("MIME type: {}".format(mime_type))
 
-def crawl_one_and_expand(url, queue, archive, external_links):
-    if url in archive:
-        return
-    text, links = crawl_website(url)
-    archive[url] = text
+        # Get the response code
+        response_code = response.status_code
+        if response_code != 200:
+            return "RESPONSE CODE IS {} for {}".format(response_code, url), []
 
-    print("Archive size:", len(archive))
-    print(url)
-    print(text)
-    print('=' * 80)
+        # Parse the HTML content using BeautifulSoup
+        soup = BeautifulSoup(response.content, "html.parser")
 
-    for l in links:
-        # base_url = SEED
-        relative_path = l
-        full_url = urljoin(url, relative_path)
-        full_url = sanitize_url(full_url)
-        if not is_interesting(full_url):
-            external_links.add(full_url)
-            continue
+        # Extract all text from the HTML content
+        text = soup.get_text()
+        links = [link.get("href") for link in soup.find_all("a")]
 
-        if full_url in archive:
-            continue
+        return text, links
 
-        queue.append(full_url)
-        # print(full_url)  # Output: https://example.com/path/to/resource
+    def crawl_one_and_expand(self, url):
+        queue = self.queue
+        archive = self.archive
+        external_links = self.external_links
 
-def crawl_nuro():
-    queue = [SEED]
-    archive = {}
-    external_links = set()
-    while queue:
-        url = queue.pop()
-        crawl_one_and_expand(url, queue, archive, external_links)
+        if url in archive:
+            return
+        text, links = self.crawl_website(url)
+        archive[url] = text
 
-    external_links = sorted(external_links)
-    for el in external_links:
-        print(el)
+        print("Archive size:", len(archive))
+        print(url)
+        print(text)
+        print("=" * 80)
+
+        for l in links:
+            # base_url = SEED
+            relative_path = l
+            full_url = urljoin(url, relative_path)
+            full_url = self._sanitize_url(full_url)
+            if not self._is_interesting(full_url):
+                external_links.add(full_url)
+                continue
+
+            if full_url in archive:
+                continue
+
+            self.queue.append(full_url)
+
+
+NURO_SEED = "https://nuro.ai"
+AURORA_SEED = "https://aurora.tech/"
+
+NURO_PREFIX = [
+    "https://medium.com/nuro",
+    "https://nuro.ai",
+    "https://www.nuro.ai",
+    "http://nuro.ai",
+    "http://www.nuro.ai",
+]
+
 
 def main():
-    crawl_nuro()
+    nuro_crawler = Crawler(NURO_SEED, NURO_PREFIX)
+    nuro_crawler.crawl()
 
-if __name__ == '__main__':
+
+if __name__ == "__main__":
     main()
