edf8bd7703d7cbfeb61068be813d812468e3914b
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-08 22:09:13 -0700

    image-classification dir

--- ml/image-classification/dataloader.py
+++ ml/image-classification/dataloader.py
@@ -1,9 +1,28 @@
 from tensorflow.keras.datasets import mnist, fashion_mnist
-# other ways to load MNIST include using torchvision, or sklearn.
+from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.utils import to_categorical
 from loguru import logger
 
 
-def load_mnist(fashion=False):
+def load_cifar10(onehot=False):
+    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
+    logger.info("Using cifar10 dataset")
+
+    image_shape = x_train[0].shape  # (32, 32)
+    logger.info(f"The size is {len(x_train)}")
+    logger.info(f"The shape is: {image_shape}")
+
+    if onehot:
+        y_train = to_categorical(y_train)  # Convert the labels to one-hot encoding
+        y_test = to_categorical(y_test)
+
+    # Normalize the data
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    return x_train, y_train, x_test, y_test
+
+
+def load_mnist(fashion=False, onehot=False):
     # Load the MNIST dataset
     if fashion:
         (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
@@ -12,11 +31,20 @@ def load_mnist(fashion=False):
         (x_train, y_train), (x_test, y_test) = mnist.load_data()
         logger.info("Using MNIST dataset")
 
-    image_shape = x_train[0].shape # (28, 28)
+    image_shape = x_train[0].shape  # (28, 28)
+    logger.info(f"The size is {len(x_train)}")
     logger.info(f"The shape is: {image_shape}")
+
+    if onehot:
+        y_train = to_categorical(y_train)  # Convert the labels to one-hot encoding
+        y_test = to_categorical(y_test)
 
     # Normalize the data
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
     return x_train, y_train, x_test, y_test
 
+
+if __name__ == "__main__":
+    load_mnist()
+    load_cifar10()
---
+++ ml/image-classification/general_test.py
@@ -0,0 +1,42 @@
+import dataloader
+from tensorflow.keras import layers, models
+from loguru import logger
+import click
+
+
+def train_eval(x_train, y_train, x_test, y_test, onehot):
+    image_shape = x_train[0].shape
+    if onehot:
+        num_classes = y_train.shape[1]
+    else:
+        num_classes = max(y_train) + 1
+    logger.info(f"Number of classes: {num_classes}")
+
+    model = models.Sequential(
+        [
+            layers.Flatten(input_shape=image_shape),
+            layers.Dense(64, activation="relu"),
+            layers.Dense(num_classes, activation="softmax"),
+        ]
+    )
+    loss_fn = (
+        "categorical_crossentropy" if onehot else "sparse_categorical_crossentropy"
+    )
+    model.compile(optimizer="adam", loss=loss_fn, metrics=["accuracy"])
+    history = model.fit(x_train, y_train, epochs=4, batch_size=64, validation_split=0.2)
+
+
+@click.command()
+@click.option(
+    "--onehot/--no-onehot", default=False, help="Whether to use one-hot encoding or not"
+)
+def main(onehot):
+    # Use the `onehot` variable here to determine whether to use one-hot encoding or not
+    print(f"Using one-hot encoding: {onehot}")
+    train_eval(*(dataloader.load_mnist(onehot=onehot)), onehot=onehot)
+    train_eval(*(dataloader.load_mnist(fashion=True, onehot=onehot)), onehot=onehot)
+    train_eval(*(dataloader.load_cifar10(onehot=onehot)), onehot=onehot)
+
+
+if __name__ == "__main__":
+    main()
