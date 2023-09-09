bf23bf997460e31969e7229937ffb0e08f85d64f
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-08 20:02:38 -0700

    -

--- ml/mnist/dataloader.py
+++ ml/mnist/dataloader.py
@@ -1,4 +1,5 @@
 from tensorflow.keras.datasets import mnist, fashion_mnist
+# other ways to load MNIST include using torchvision, or sklearn.
 from loguru import logger
 
 
@@ -17,3 +18,4 @@ def load_mnist(fashion=False):
     x_train, x_test = x_train / 255.0, x_test / 255.0
 
     return x_train, y_train, x_test, y_test
+
