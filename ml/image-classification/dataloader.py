b84830432f15065c1cb6c30bd2cb8c6e3b6866ff
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-08 22:34:35 -0700

    cifar100 loader and refactor

--- ml/image-classification/dataloader.py
+++ ml/image-classification/dataloader.py
@@ -1,48 +1,46 @@
 from tensorflow.keras.datasets import mnist, fashion_mnist
-from tensorflow.keras.datasets import cifar10
+from tensorflow.keras.datasets import cifar10, cifar100
 from tensorflow.keras.utils import to_categorical
 from loguru import logger
+from functools import partial
+
+
+def _load(loader, name, onehot):
+    (x_train, y_train), (x_test, y_test) = loader()
+    logger.info(f"Using {name} dataset")
+
+    image_shape = x_train[0].shape
+    logger.info(f"The size is {len(x_train)}")
+    logger.info(f"The shape is: {image_shape}")
+
+    if onehot:
+        y_train = to_categorical(y_train)  # Convert the labels to one-hot encoding
+        y_test = to_categorical(y_test)
+
+    # Normalize the data
+    x_train, x_test = x_train / 255.0, x_test / 255.0
+
+    return x_train, y_train, x_test, y_test
 
 
 def load_cifar10(onehot=False):
-    (x_train, y_train), (x_test, y_test) = cifar10.load_data()
-    logger.info("Using cifar10 dataset")
+    # Shape: (32, 32, 3)
+    return _load(cifar10.load_data, "cifar10", onehot)
 
-    image_shape = x_train[0].shape  # (32, 32)
-    logger.info(f"The size is {len(x_train)}")
-    logger.info(f"The shape is: {image_shape}")
-
-    if onehot:
-        y_train = to_categorical(y_train)  # Convert the labels to one-hot encoding
-        y_test = to_categorical(y_test)
-
-    # Normalize the data
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    return x_train, y_train, x_test, y_test
+def load_cifar100(onehot=False):
+    # Shape: (32, 32, 3)
+    return _load(cifar100.load_data, "cifar100", onehot)
 
 
-def load_mnist(fashion=False, onehot=False):
-    # Load the MNIST dataset
-    if fashion:
-        (x_train, y_train), (x_test, y_test) = fashion_mnist.load_data()
-        logger.info("Using fashion dataset")
-    else:
-        (x_train, y_train), (x_test, y_test) = mnist.load_data()
-        logger.info("Using MNIST dataset")
+def _load_mnist(fashion=False, onehot=False):
+    # Shape: (28, 28)
+    data = fashion_mnist if fashion else mnist
+    name = "fashion" if fashion else "mnist"
+    return _load(data.load_data, name, onehot)
 
-    image_shape = x_train[0].shape  # (28, 28)
-    logger.info(f"The size is {len(x_train)}")
-    logger.info(f"The shape is: {image_shape}")
 
-    if onehot:
-        y_train = to_categorical(y_train)  # Convert the labels to one-hot encoding
-        y_test = to_categorical(y_test)
-
-    # Normalize the data
-    x_train, x_test = x_train / 255.0, x_test / 255.0
-
-    return x_train, y_train, x_test, y_test
+load_mnist = partial(_load_mnist, fashion=False)
+load_fashion_mnist = partial(_load_mnist, fashion=True)
 
 
 if __name__ == "__main__":
