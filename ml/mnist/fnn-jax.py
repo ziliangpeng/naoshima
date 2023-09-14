7738b961a9b9d05722a80ef9557e0bafa16de836
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-13 21:48:00 -0700

    fix jax to compare

--- ml/mnist/fnn-jax.py
+++ ml/mnist/fnn-jax.py
@@ -5,6 +5,7 @@ from jax import random
 
 """ Only running on CPU. Have not figured out how to run JAX on Metal (Apple GPU)"""
 
+
 # Define the neural network architecture
 def init_params(rng):
     w1 = jnp.array(random.normal(rng, (784, 128)))
@@ -15,6 +16,7 @@ def init_params(rng):
     b3 = jnp.zeros((10,))
     return (w1, b1, w2, b2, w3, b3)
 
+
 def predict(params, x):
     w1, b1, w2, b2, w3, b3 = params
     x = jnp.reshape(x, (x.shape[0], -1))
@@ -22,23 +24,27 @@ def predict(params, x):
     hidden2 = jnp.dot(jax.nn.relu(hidden1), w2) + b2
     return jnp.dot(jax.nn.relu(hidden2), w3) + b3
 
+
 # Define the loss function
 def loss(params, images, labels):
     preds = predict(params, images)
     return -jnp.mean(jax.nn.log_softmax(preds) * labels)
+
 
 # Define the accuracy function
 def accuracy(params, images, labels):
     preds = predict(params, images)
     return jnp.mean(jnp.argmax(preds, axis=1) == jnp.argmax(labels, axis=1))
 
+
 # Load the MNIST dataset
 from tensorflow.keras.datasets import mnist
+
 (train_images, train_labels), (test_images, test_labels) = mnist.load_data()
 
 # Normalize the pixel values
-train_images = train_images.astype('float32') / 255
-test_images = test_images.astype('float32') / 255
+train_images = train_images.astype("float32") / 255
+test_images = test_images.astype("float32") / 255
 
 # One-hot encode the labels
 train_labels = jax.nn.one_hot(train_labels, 10)
@@ -48,16 +54,18 @@ test_labels = jax.nn.one_hot(test_labels, 10)
 rng = random.PRNGKey(0)
 params = init_params(rng)
 
+
 # Define the optimizer
 @jax.jit
 def update(params, x, y, lr):
     grads = jax.grad(loss)(params, x, y)
     return [(param - lr * grad) for param, grad in zip(params, grads)]
 
+
 # Train the neural network
-batch_size = 64
-num_epochs = 42
-learning_rate = 0.001
+batch_size = 128
+num_epochs = 10
+learning_rate = 4e-3
 num_batches = train_images.shape[0] // batch_size
 
 for epoch in range(num_epochs):
@@ -71,4 +79,4 @@ for epoch in range(num_epochs):
 
     # Evaluate the neural network on the test set
     test_acc = accuracy(params, test_images, test_labels)
-    print('Epoch: {} Test accuracy: {:.4f}'.format(epoch, test_acc))
+    print("Epoch: {} Test accuracy: {:.4f}".format(epoch, test_acc))
--- ml/objcla/main_jax_fnn.py
+++ ml/objcla/main_jax_fnn.py
@@ -1,4 +1,3 @@
-import time
 import jax
 import dataloader
 from jax import random
@@ -8,74 +7,48 @@ import operator
 from loguru import logger
 import click
 
-num_filters = 8
 
-
-def fast_cnn(params, images, num_filters):
-    conv_w, conv_b = params
-    conved = jnp.zeros((images.shape[0], 26, 26, num_filters))
-    for i in range(26):
-        for j in range(26):
-            conved = conved.at[:, i, j, :].set(
-                jnp.sum(images[:, i : i + 3, j : j + 3, jnp.newaxis] * conv_w[:, :, :], axis=(1, 2)) + conv_b[:]
-            )
-    return conved
-
-# @jax.jit
 def predict_batch(params, inputs):
-    conv_w, conv_b, w1, b1 = params
-    conved = fast_cnn((conv_w, conv_b), inputs, num_filters)
+    inputs = jnp.reshape(inputs, (inputs.shape[0], -1))
 
-    conved = jnp.reshape(conved, (conved.shape[0], -1))
-    logits = jnp.dot(conved, w1) + b1
-    # logger.info('done train')
+    w1, b1 = params
+    logits = jnp.dot(inputs, w1) + b1
     return logits
 
 
-# @jax.jit
 def correct_batch(params, inputs, targets):
     preds = predict_batch(params, inputs)
-    # print(preds[0])
-    # print(jnp.argmax(preds[0]))
-    # print(jnp.argmax(preds, axis=1))
-    # print(jnp.argmax(targets, axis=1))
-    num_correct = jnp.sum(jnp.argmax(preds, axis=1) == jnp.argmax(targets, axis=1))
-    logger.info(f"num_correct: {num_correct} / {inputs.shape[0]}")
-    # return jnp.sum(jnp.argmax(preds, axis=1) == jnp.argmax(targets, axis=1))
-    return num_correct
+    return jnp.sum(jnp.argmax(preds, axis=1) == jnp.argmax(targets, axis=1))
 
 
-# @jax.jit
 def loss_batch(params, inputs, targets):
     preds = predict_batch(params, inputs)
     l = -jnp.mean(jax.nn.log_softmax(preds) * targets)
-    # logger.info(f"loss: {jnp.asarray(l)}")
-    # logger.info(l.shape)
     return l
 
 
-# @jax.jit
+@jax.jit
+def update_batch(params, x, y, lr):
+    grads = jax.grad(loss_batch)(params, x, y)
+    return [(param - lr * grad) for param, grad in zip(params, grads)]
+
+
 def train_batch(x_train, y_train, x_test, y_test, params, lr):
-    batch_size = 512
+    batch_size = 128
     num_batches = x_train.shape[0] // batch_size
-    epochs = 10000
+    epochs = 10
     for epoch in range(epochs):
-        start_time = time.time()
         for i in range(num_batches):
             start_idx = i * batch_size
             end_idx = (i + 1) * batch_size
             inputs = x_train[start_idx:end_idx]
             labels = y_train[start_idx:end_idx]
-            grads = jax.grad(loss_batch)(params, inputs, labels)
-            # logger.info('got grads')
-            params = [(param - lr * grad) for param, grad in zip(params, grads)]
+            params = update_batch(params, inputs, labels, lr)
 
-        end_train_time = time.time()
         corrects = correct_batch(params, x_train, y_train)
         valid_corrects = correct_batch(params, x_test, y_test)
-        end_time = time.time()
         logger.info(
-            f"Epoch {epoch}, train acc {corrects / x_train.shape[0]:.3f}, valid acc {valid_corrects / x_test.shape[0]:.3f}, train time {end_train_time - start_time:.3f}, eval time {end_time - end_train_time:.3f}"
+            f"Epoch {epoch}, train acc {corrects / x_train.shape[0]:.3f}, valid acc {valid_corrects / x_test.shape[0]:.3f}"
         )
 
 
@@ -84,14 +57,6 @@ def main():
     # loader = dataloader.load_cifar10
     loader = dataloader.load_mnist
     x_train, y_train, x_test, y_test = loader(onehot=True)
-    # At least it can train and overfit on a small dataset.
-    num_images = 600 # 6000 * 3
-    num_vad = 100
-    x_train = x_train[:num_images]
-    y_train = y_train[:num_images]
-    x_test = x_test[:num_vad]
-    y_test = y_test[:num_vad]
-    logger.info(f"training sample {num_images}, validation sample {num_vad}")
 
     num_classes = y_train.shape[1]
 
@@ -102,16 +67,13 @@ def main():
     rng = random.PRNGKey(0)
 
     def init_params(rng):
-        conv_w = jnp.array(random.normal(rng, (3, 3, num_filters)))
-        conv_b = jnp.zeros((num_filters,))
-
-        w1 = jnp.array(random.normal(rng, (26 * 26 * num_filters, num_classes)))
+        w1 = jnp.array(random.normal(rng, (FC_len, num_classes)))
         b1 = jnp.zeros((num_classes,))
-        return (conv_w, conv_b, w1, b1)
+        return (w1, b1)
 
     params = init_params(rng)
 
-    lr = 0.01
+    lr = 4e-3
     train_batch(x_train, y_train, x_test, y_test, params, lr)
 
 
