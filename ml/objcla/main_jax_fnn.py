9d692a880895158dfac39a4057ddb5cc8c5c0bc1
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-13 22:23:40 -0700

    adds a non-linear NN for jax fnn

--- ml/objcla/main_jax_fnn.py
+++ ml/objcla/main_jax_fnn.py
@@ -8,12 +8,28 @@ from loguru import logger
 import click
 
 
-def predict(params, inputs):
+def predict1(params, inputs):
     inputs = jnp.reshape(inputs, (inputs.shape[0], -1))
 
-    w1, b1 = params
-    logits = jnp.dot(inputs, w1) + b1
+    w1, b1, w2, b2 = params
+    num_classes = w2.shape[1]
+    logits = jnp.dot(inputs, w1[:, :num_classes]) + b1[:num_classes]
     return logits
+
+
+def predict2(params, inputs):
+    inputs = jnp.reshape(inputs, (inputs.shape[0], -1))
+
+    w1, b1, w2, b2 = params
+    hidden1 = jnp.dot(inputs, w1) + b1
+    logits = jnp.dot(jax.nn.relu(hidden1), w2) + b2
+    return logits
+
+
+# predict1 is a single layer network, no relu, and thus a linear classifier
+# predict2 is a two layer network, with relu, and thus a non-linear classifier
+# predict = predict1
+predict = predict2
 
 
 def correct(params, inputs, targets):
@@ -67,9 +83,12 @@ def main():
     rng = random.PRNGKey(0)
 
     def init_params(rng):
-        w1 = jnp.array(random.normal(rng, (FC_len, num_classes)))
-        b1 = jnp.zeros((num_classes,))
-        return (w1, b1)
+        w1 = jnp.array(random.normal(rng, (FC_len, 128)))
+        b1 = jnp.zeros((128,))
+
+        w2 = jnp.array(random.normal(rng, (128, num_classes)))
+        b2 = jnp.zeros((num_classes,))
+        return (w1, b1, w2, b2)
 
     params = init_params(rng)
 
