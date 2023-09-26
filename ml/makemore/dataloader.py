f28026563c67aa41bf1d159ad095751b31d1ae15
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-25 18:53:04 -0700

    tokenize names

--- ml/makemore/dataloader.py
+++ ml/makemore/dataloader.py
@@ -9,7 +9,18 @@ def load_names():
     return names
 
 
+def load_tokenized_names(names, pair_len=2):
+    tokenized_names = []
+    for name in names:
+        tokens = []
+        for i in range(0, len(name) - pair_len + 1):
+            tokens.append(name[i : i + pair_len])
+        tokenized_names.append(tokens)
+    return tokenized_names
+
+
 if __name__ == "__main__":
     names = load_names()
     print(names[:10])
     print(len(names))
+    print(load_tokenized_names(names[:2]))
