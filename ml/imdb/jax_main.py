16bb05801c178cbbb96c75e4d2311f8b4e08495d
Ziliang Peng <ziliangdotme@gmail.com>
2023-09-29 19:05:48 -0700

    jax can solve imdb now

--- ml/imdb/jax_main.py
+++ ml/imdb/jax_main.py
@@ -10,52 +10,41 @@ train_texts = X_train
 train_labels = y_train
 
 input_dim = 250
-embedding_dim = 128
-hidden_dim = 64
-output_dim = 1
+embedding_dim = 32
 
 key = random.PRNGKey(0)
-# params = {
-#     "w1": random.normal(key, (input_dim, hidden_dim)),
-#     "b1": jnp.zeros(hidden_dim),
-#     "w2": random.normal(key, (hidden_dim, output_dim)),
-#     "b2": jnp.zeros(output_dim),
-# }
+# simple MLP can achieve good result, and after running long time I can get 80+% accuracy.
+# TODO: I need a good optimizer.
 params = {
     # Embedding layer
     "w1": random.normal(key, (VOCAB_SIZE, embedding_dim)),
-    # "b1": jnp.zeros(output_dim),
-
-    "w2": random.normal(key, (MAX_LENGTH * embedding_dim, hidden_dim)),
-    "b2": jnp.zeros(hidden_dim),
-
-    "w3": random.normal(key, (hidden_dim, output_dim)),
-    "b3": jnp.zeros(output_dim),
+    # MLP
+    "w2": random.normal(key, (input_dim * embedding_dim, 32)),
+    "b2": jnp.zeros(32),
+    "w3": random.normal(key, (32, 1)),
+    "b3": jnp.zeros(1),
 }
 
 
-# def model(params, x):
-#     x = jnp.dot(x, params["w1"]) + params["b1"]
-#     x = jnp.tanh(x)
-#     x = jnp.dot(x, params["w2"]) + params["b2"]
-#     return x
-
 def model(params, x):
-    # Embedding layer.
+    ## Embedding layer.
     # x: (n, length) => (n, length, embedding_dim)
     embedding_matrix = params["w1"]
     x = embedding_matrix[x]
     # print(x.shape)
-    
+
+    ## GlobalAveragePooling1D
+    # x = x.mean(axis=1)
+
+    ## Flatten
     x = x.reshape((x.shape[0], -1))
     # print(x.shape)
 
-    # x = jnp.dot(x, params["w1"]) + params["b1"]
-    # x = jnp.tanh(x)
+    ## MLP
     x = jnp.dot(x, params["w2"]) + params["b2"]
     x = jnp.tanh(x)
-
     x = jnp.dot(x, params["w3"]) + params["b3"]
+    x = jax.nn.sigmoid(x)
     return x
 
 
@@ -73,9 +62,15 @@ def update(params, x, y):
     return new_params
 
 
-for epoch in range(42):
+for epoch in range(10000):
     # print(train_texts.shape, train_labels.shape)
-    params = update(params, train_texts, train_labels)
+    for batch in range(0, len(train_texts), 32):
+        params = update(
+            params,
+            train_texts[batch : batch + 32],
+            train_labels[batch : batch + 32],
+        )
+    # params = update(params, train_texts, train_labels)
     train_loss = loss(params, train_texts, train_labels)
     # print(f"Epoch {epoch+1}, Loss: {train_loss}")
 
@@ -84,7 +79,6 @@ for epoch in range(42):
     correct = 0
     for data, label in zip(train_texts, train_labels):
         total += 1
-        # print(data.shape)
-        # print(data.reshape(1, -1).shape)
-        correct += (0 if model(params, data.reshape(1, -1)) < 0 else 1) == label
+        correct += (0 if model(params, data.reshape(1, -1)) < 0.5 else 1) == label
     print(f"Train Accuracy: {correct / total}")
+    # print(f"Correct: {correct}, Total: {total}")
