c393eb06f729265aac44714ef94b382e9276362f
Ziliang Peng <ziliangdotme@gmail.com>
2023-10-07 14:20:15 -0700

    a v script for video

--- codec/mdate.py
+++ codec/mdate.py
@@ -7,24 +7,28 @@ from datetime import datetime
 import calendar
 import sys
 
-filename = 'DJI_0550_001.MP4-converted-br-33177600.mp4'
-filename = sys.argv[1]
 
-stream = ff.input(filename)
+def change_date(filename):
+    # stream = ff.input(filename)
 
-cdate_str = ''
+    cdate_str = ""
 
-for s in ff.probe(filename)["streams"]:
-  if s['codec_type'] == 'video':
-    cdate_str = s['tags']['creation_time']
-assert cdate_str
+    for s in ff.probe(filename)["streams"]:
+        if s["codec_type"] == "video":
+            cdate_str = s["tags"]["creation_time"]
+    assert cdate_str
 
-print(cdate_str)
+    print(cdate_str)
 
-d = datetime.strptime(cdate_str.split('.')[0], '%Y-%m-%dT%H:%M:%S')
-print(d)
-epoch = calendar.timegm(d.timetuple())
-print(epoch)
+    d = datetime.strptime(cdate_str.split(".")[0], "%Y-%m-%dT%H:%M:%S")
+    print(d)
+    epoch = calendar.timegm(d.timetuple())
+    print(epoch)
 
-utime(filename, (epoch, epoch))
+    utime(filename, (epoch, epoch))
 
+
+if __name__ == "__main__":
+    # filename = 'DJI_0550_001.MP4-converted-br-33177600.mp4'
+    fn = sys.argv[1]
+    change_date(fn)
