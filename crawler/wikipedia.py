b64ab897efbf2af9ab84f4ea5d1e1f013bb80ac5
Ziliang Peng <ziliangdotme@gmail.com>
2023-10-27 14:44:12 -0700

    decode url for wiki. dedup links.

--- crawler/wikipedia.py
+++ crawler/wikipedia.py
@@ -8,13 +8,25 @@ from collections import defaultdict
 
 from loguru import logger
 
+import urllib.parse
+
 WIKI_PAGES_DIR = 'wiki-pages'
 
 START_URL = "https://en.wikipedia.org/wiki/Python_(programming_language)"
+# 阿姆斯特丹
 START_URL = "https://zh.wikipedia.org/wiki/%E9%98%BF%E5%A7%86%E6%96%AF%E7%89%B9%E4%B8%B9"
+# 人工神经网络
+START_URL = "https://zh.wikipedia.org/wiki/%E4%BA%BA%E5%B7%A5%E7%A5%9E%E7%BB%8F%E7%BD%91%E7%BB%9C"
+
+
+def decode_url(url):
+    decoded_bytes = urllib.parse.unquote_to_bytes(url)
+    decoded_text = decoded_bytes.decode('utf-8', errors='replace')
+    return decoded_text
+
 
 def get_wiki_page(url):
-    logger.info(f"Crawling {url}")
+    logger.info(f"Crawling {decode_url(url)}")
     response = requests.get(url)
     page_content = response.content
     soup = BeautifulSoup(page_content, 'html.parser')
@@ -33,13 +45,14 @@ def get_wiki_page(url):
 
     # Find links to other Wikipedia pages
     links = [a['href'] for a in soup.find_all('a', href=True) if 'wiki' in a['href'] and ':' not in a['href']]
-    pages = []
+    # TODO: a flag to control whether to use set or list.
+    pages = set([])
     for link in links:
         if not link.startswith('/wiki/'):
             continue
         next_page = f'{host}{link}'
         # print(f'Next: {next_page}')
-        pages.append(next_page)
+        pages.add(next_page)
         # time.sleep(1)  # Sleep for 1 second to be respectful to Wikipedia's servers
         # crawl_wikipedia(next_page, depth-1)
 
@@ -48,7 +61,9 @@ def get_wiki_page(url):
 def find_top_k(queue, k):
     ret =  sorted(queue, key=lambda x: queue[x], reverse=True)[:k]
     for k in ret:
-        logger.info(f"{k}: {queue[k]}")
+        item = k[k.find('wiki/')+len('wiki/'):]
+        item = decode_url(item)
+        logger.info(f"{item}: {queue[k]}")
     # logger.info(ret)
     return ret
 
