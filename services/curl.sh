e05e840c582cd94d71773747aa782a7fe506599f
Ziliang Peng <ziliangdotme@gmail.com>
2024-07-27 17:45:27 -0700

    some improvements

--- services/curl.sh
+++ services/curl.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-curl -X POST -H "Content-Type: application/json" -d '{"input_integer": 42}' http://127.0.0.1:4200/echo
+curl -vv -X POST -H "Content-Type: application/json" -d '{"input_integer": 42}' http://127.0.0.1:4200/echo
--- services/server.py
+++ services/server.py
@@ -1,4 +1,5 @@
 from flask import Flask, request, jsonify
+from flask_caching import Cache
 
 import statsd
 
@@ -6,9 +7,15 @@ sd = statsd.StatsClient('localhost', 8125)
 
 # curl -X POST -H "Content-Type: application/json" -d '{"input_integer": 42}' http://127.0.0.1:4200/echo
 
+# Run with gunicorn:
+# gunicorn --workers 4 --bind 0.0.0.0:4200 app:app
+
 app = Flask(__name__)
+cache = Cache(app, config={'CACHE_TYPE': 'simple'})
 
 @app.route('/echo', methods=['POST'])
+# @cache.cached(timeout=60)
+@cache.cached()
 def echo_integer():
     data = request.get_json()
     if 'input_integer' not in data:
